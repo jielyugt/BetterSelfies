63d84bcaf0795a499dbc9f0d49afc7e086580a60
jielyugt <jie.lyu@gatech.edu>
2019-03-31 00:21:03 -0400

    somemore

--- MyPlayground.playground/Contents.swift
+++
@@ -1,6 +0,0 @@
-// Import CreateMLUI to train the image classifier in the UI.
-// For other Create ML tasks, import CreateML instead.
-import CreateMLUI
-
-let builder = MLImageClassifierBuilder()
-builder.showInLiveView()
